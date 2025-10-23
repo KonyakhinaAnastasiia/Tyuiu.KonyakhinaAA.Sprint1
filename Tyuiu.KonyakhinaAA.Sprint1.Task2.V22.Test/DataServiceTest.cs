f9f6d2ea9ddd77414f4ea61d70e2e06ab9d2704c
KonyakhinaAnastasiia <the.nobodies749.2@gmail.com>
2025-10-23 21:57:44 +0500

    Task2

---
+++ Tyuiu.KonyakhinaAA.Sprint1.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KonyakhinaAA.Sprint1.Task2.V22.Lib;
+
+namespace Tyuiu.KonyakhinaAA.Sprint1.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            var res = ds.CalculateAVGValue(a, b, c);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
